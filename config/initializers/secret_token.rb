bf379a8ef3863ac150a967ba71166a086daf7d21
Ryan Mawby <ryan@wegoodhq.com>
2014-01-29 17:44:20 +0000

    Mods

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieData::Application.config.secret_key_base = '96ebdcb437b73deaf7cceaf04a9c4622adb4a56efaac9d8e3f9b64cf2c3a91d59d418c337a3cc82fabb66d73a45eaf3196450ea78eaa2a7268d18669f569959c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovieData::Application.config.session_store :cookie_store, key: '_MovieData_session'
